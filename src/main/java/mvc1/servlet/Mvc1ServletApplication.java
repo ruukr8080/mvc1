81cd19ca04b46b2f5c5aebac72b6aefadf6c0ffc
ruukr8080 <ruukr8080@gmail.com>
2024-02-12 12:22:36 +0900

    MVC exercise the first commit

---
+++ src/main/java/mvc1/servlet/Mvc1ServletApplication.java
@@ -0,0 +1,13 @@
+package mvc1.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc1ServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mvc1ServletApplication.class, args);
+    }
+
+}
